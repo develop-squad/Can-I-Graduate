b2303c49ea77a19e272423c09feb9074710359ba
MoonJuhan <codemike@naver.com>
2020-11-21 16:29:53 +0900

    feat - 레이아웃 기능 추가

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="sidebar">
+    <ul>
+      <li v-bind:class="tab == 'dashboard' ? 'selected' : ''" @click="onClickTab('dashboard')">Dashboard</li>
+      <li v-bind:class="tab == 'menu2' ? 'selected' : ''" @click="onClickTab('menu2')">Menu 02</li>
+      <li v-bind:class="tab == 'menu3' ? 'selected' : ''" @click="onClickTab('menu3')">Menu 03</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { onMounted } from "vue";
+
+export default {
+  props: {
+    tab: String,
+    changeTab: Function,
+  },
+  setup(props) {
+    onMounted(() => {
+      // console.log(props);
+    })
+
+    const onClickTab = (param) => {
+      props.changeTab(param);
+    };
+
+    return {
+      onClickTab
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.sidebar {
+  margin: 0 15px;
+  background-color: #28273f;
+  border-radius: 4px;
+
+  & > ul {
+    padding: 10px;
+
+    & > li {
+      margin: 5px;
+      padding: 5px;
+      cursor: pointer;
+    }
+
+    & > li.selected {
+      font-weight: bold;
+    }
+  }
+}
+</style>
---
+++ src/layouts/DashboardLayout.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="layout">
+    <layout-header :tab="state.tab" />
+    <div class="main-container">
+      <sidebar :tab="state.tab" :changeTab="changeTab"></sidebar>
+      <router-view class="contents" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Sidebar from "../components/Sidebar";
+import LayoutHeader from "../components/LayoutHeader";
+import { reactive } from "vue";
+import router from "../router";
+
+export default {
+  components: {
+    Sidebar,
+    LayoutHeader,
+  },
+  setup() {
+    const state = reactive({
+      tab: "dashboard",
+    });
+
+    const changeTab = (tab) => {
+      state.tab = tab;
+      switch (tab) {
+        case "dashboard":
+          router.push("/");
+          break;
+        case "menu2":
+          router.push("menu2");
+          break;
+
+        case "menu3":
+          router.push("menu3");
+          break;
+      }
+    };
+
+    return {
+      state,
+      changeTab,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.layout {
+  display: flex;
+  flex-wrap: wrap;
+
+  & > .main-container {
+    display: flex;
+    width: 100%;
+  }
+
+  & > .main-container > .sidebar {
+    width: 20%;
+  }
+
+  & > .main-container > .contents {
+    width: 80%;
+  }
+}
+</style>
