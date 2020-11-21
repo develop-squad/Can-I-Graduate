16a82bcfa4b32818da6e8a230c1fc78277240bd9
MoonJuhan <codemike@naver.com>
2020-11-21 16:30:03 +0900

    feat - 대시보드 페이지 추가

---
+++ src/pages/Dashboard.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <button @click="onClickBtn">Clicked {{ count }} times.</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+export default defineComponent({
+  setup() {
+    const count = ref(0);
+    const onClickBtn = () => {
+      // console.log(defineComponent);
+      count.value++;
+    };
+
+    return {
+      count,
+      onClickBtn,
+    };
+  },
+});
+</script>
